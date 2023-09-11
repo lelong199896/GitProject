05dc5afa3cdff0d7f0d4134b0962b69f6aba2fc6
lelong199896 <lelong199896@gmail.com>
2023-09-11 14:54:16 +0700

    THUseOperator

---
+++ TH-Sử dụng toán tử/operator.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("The area is: " + area);
